4bd5061d3f2f8e15e5daa4f5cac36016fb44b03c
Natalia Dombrowska <ndombrowska@scottlogic.com>
2021-11-18 13:29:06 +0000

    add counter feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Counter></Counter>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
